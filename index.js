88a3b9a097e8c01589eb33d90bd0b053e07319d9
Matthew Butler <matthewtbutler@gmail.com>
2023-07-25 22:38:39 -0500

    class refactor

--- index.js
+++ index.js
@@ -17,127 +17,141 @@ class Node {
     }
 }
 
-function insertNode(nodes, nodeObj) {
-    let node = new Node(nodeObj.id, nodeObj.properties);
-    nodes[node.id] = node;
-}
-
-function loadNodesFromFile(filePath, nodes) {
-    let data = fs.readFileSync(filePath);
-    let jsonData = JSON.parse(data);
-
-    jsonData.forEach(obj => {
-        if (obj.type === "node") {
-            insertNode(nodes, obj);
-        }
-    });
-}
-
-function loadRelationshipsFromFile(filePath, nodes) {
-    let data = fs.readFileSync(filePath);
-    let jsonData = JSON.parse(data);
-
-    jsonData.forEach(obj => {
-        if (obj.type === "relationship" && obj.label === "Proves") {
-            let startNode = nodes[obj.start.id];
-            let endNode = nodes[obj.end.id];
-
-            if (startNode && endNode) {
-                startNode.addProof(endNode);
-                endNode.addProofFor(startNode);
-            }
-        }
-    });
-}
-
-function getNodeByNumber(nodes, number) {
-    for (let id in nodes) {
-        let node = nodes[id];
-        if (node.properties.number === number) {
-            return node;
+class Graph {
+    constructor(filePath) {
+        this.nodes = {};
+        if (filePath) {
+            this._loadJson(filePath);
         }
     }
-    return null;
-}
 
-function getNodeById(nodes, id) {
-    return nodes[id] || null;
-}
+    _loadJson(filePath) {
+        let data = fs.readFileSync(filePath);
+        let jsonData = JSON.parse(data);
+    
+        jsonData.forEach(obj => {
+            if (obj.type === "node") {
+                this._insertNode(obj);
+            } else if (obj.type === "relationship" && obj.label === "Proves") {
+                let startNode = this.nodes[obj.start.id];
+                let endNode = this.nodes[obj.end.id];
+    
+                if (startNode && endNode) {
+                    startNode.addProof(endNode);
+                    endNode.addProofFor(startNode);
+                }
+            }
+        });
+    }
 
-function getParent(nodes, node) {
-    // Split the number into parts
-    let parts = node.properties.number.split('.');
+    _insertNode(obj) {
+        let node = new Node(obj.id, obj.properties);
+        this.nodes[node.id] = node;
+    }
 
-    // If the node is a root node, it has no parent
-    if (parts.length < 2) {
+    getNodeByNumber(number) {
+        for (let id in this.nodes) {
+            let node = this.nodes[id];
+            if (node.properties.number === number) {
+                return node;
+            }
+        }
         return null;
     }
 
-    // Remove the last part to get the parent's number
-    let parentNumber = parts.slice(0, parts.length - 1).join('.');
-
-    return getNodeByNumber(nodes, parentNumber);
-}
-
-function getChildren(nodes, node) {
-    let children = [];
-    let childNumberPrefix = node.properties.number + '.';
-    for (let id in nodes) {
-        let childNode = nodes[id];
-        if (childNode.properties.number.startsWith(childNumberPrefix)) {
-            children.push(childNode);
-        }
+    getNodeById(id) {
+        return this.nodes[id] || null;
     }
-    return children;
-}
 
-function getChildren(nodes, node) {
-    let children = [];
-    let childNumberPrefix = node.properties.number + '.';
-    for (let id in nodes) {
-        let childNode = nodes[id];
-        if (childNode.properties.number.startsWith(childNumberPrefix)) {
-            children.push(childNode);
-        }
+    getChildrenIdsByNumber(number) {
+        let childrenIds = [];
+    
+        let numberParts = number.split('');
+    
+        Object.values(this.nodes).forEach(node => {
+            let nodeNumberParts = node.properties.number.split('');
+            let matches = nodeNumberParts.slice(0, numberParts.length).every((part, index) => part === numberParts[index]);
+    
+            if (matches && nodeNumberParts.length === numberParts.length + 1) {
+                childrenIds.push(node.id);
+            }
+        });
+    
+        // Sort by decimal number (so "2.01" comes before "2.1")
+        childrenIds.sort((a, b) => {
+            return parseFloat(this.nodes[a].properties.number) - parseFloat(this.nodes[b].properties.number);
+        });
+    
+        return childrenIds;
     }
-    return children;
-}
 
-function getChildrenIdsByNumber(nodes, number) {
-    let childrenIds = [];
-
-    let numberParts = number.split('');
-
-    Object.values(nodes).forEach(node => {
-        let nodeNumberParts = node.properties.number.split('');
-        let matches = nodeNumberParts.slice(0, numberParts.length).every((part, index) => part === numberParts[index]);
-
-        if (matches && nodeNumberParts.length === numberParts.length + 1) {
-            childrenIds.push(node.id);
+    getParentIdByNumber(number) {
+        let inputNodeExists = Object.values(this.nodes).some(node => node.properties.number === number);
+        if (!inputNodeExists) {
+            console.log(`No node exists with the number ${number}`);
+            return null;
         }
-    });
+    
+        let parentNumber;
+        let numberParts = number.split(".");
+        if (numberParts[1] && numberParts[1].length > 1) {
+            parentNumber = `${numberParts[0]}.${numberParts[1].slice(0, -1)}`;
+        }
+    
+        let parentId = null;
+        Object.values(this.nodes).forEach(node => {
+            if (node.properties.number === parentNumber) {
+                parentId = node.id;
+            }
+        });
+    
+        if (parentId === null) {
+            console.log(`No parent node exists for the number ${number}`);
+        }
+    
+        return parentId;
+    }
 
-    // Sort by decimal number (so "2.01" comes before "2.1")
-    childrenIds.sort((a, b) => {
-        return parseFloat(nodes[a].properties.number) - parseFloat(nodes[b].properties.number);
-    });
+    toJsonFile(filename) {
+        let jsonContent = JSON.stringify(this.nodes, null, 2);
+    
+        fs.writeFile(filename, jsonContent, 'utf8', function (err) {
+            if (err) {
+                console.log("An error occurred while writing JSON object to file.");
+                return console.log(err);
+            }
+            console.log("JSON file has been saved.");
+        });
+    }
 
-    return childrenIds;
+    getNodesByProperties(desiredProperties) {
+        let matchingNodes = [];
+    
+        Object.values(this.nodes).forEach(node => {
+            let matchesAll = true;
+            
+            // Check if the node contains all properties from desiredProperties
+            for (let property in desiredProperties) {
+                if (node.properties[property] !== desiredProperties[property]) {
+                    matchesAll = false;
+                    break;
+                }
+            }
+    
+            // If the node matches all desired properties, add it to matchingNodes
+            if (matchesAll) {
+                matchingNodes.push(node);
+            }
+        });
+    
+        return matchingNodes;
+    }
 }
 
-
-
-
-
-
-// Usage:
-let nodes = {};
-loadNodesFromFile('pm.json', nodes);
-loadRelationshipsFromFile('pm.json', nodes);
-
-// Usage:
-let node = getNodeByNumber(nodes, "2.521");
-let node2 = getNodeById(nodes, "50");
-let parent = getParent(nodes, node); // Returns node with number "2.5"
-let children = getChildrenIdsByNumber(nodes, "2.5"); // Returns ["2.51", "2.52", "2.53"]
+let pm = new Graph('pm.json');
+let node = pm.getNodeByNumber('2.5');
+let children = pm.getChildrenIdsByNumber('2.5');
 console.log(children);
+
+console.log(pm.getParentIdByNumber('2.37'));
+console.log(pm.getNodesByProperties({"page":"105"}));
