15878ccfe491c91b6afe93ed73037e08b8e64c01
Matthew Butler <matthewtbutler@gmail.com>
2023-09-05 13:37:09 -0500

    add search filtering inputs

--- src/datatable.js
+++ src/datatable.js
@@ -14,7 +14,27 @@ export let table = new DataTable('#principia-table', {
         { data: 'properties.volume', title: 'Volume' },
         { data: 'properties.chapter', title: 'Chapter' },
         { data: 'properties.part', title: 'Part' }
-    ]
+    ],
+    initComplete: function () {
+        this.api()
+            .columns()
+            .every(function () {
+                let column = this
+                let title = column.footer().textContent
+ 
+                // Create input element
+                let input = document.createElement('input')
+                input.placeholder = title;
+                column.footer().replaceChildren(input)
+ 
+                // Event listener for user input
+                input.addEventListener('keyup', () => {
+                    if (column.search() !== this.value) {
+                        column.search(input.value).draw()
+                    }
+                })
+            })
+    }
 })
 
 
