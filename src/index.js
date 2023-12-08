746ce20a817722a47961952367dba7af4f6e9e05
Matthew Butler <matthewtbutler@gmail.com>
2023-12-08 15:16:01 -0600

    styling improvements and fixes for bootstrap

--- src/index.js
+++ src/index.js
@@ -64,18 +64,18 @@ function normalMap() {
   })
 }
 
-$('.menu-search input').on('keydown', function(e) {
-  if (e.keyCode === 13) {
-      if ($(this).is(':focus')) {
-        const num = $(this).val()
-        const svgElement = document.getElementById('container')
-        const node = pm.getNodeByNumber(num)
-        $('html, body').animate({
-          scrollLeft: node.x - $(window).width() / 2
-      }, 100)
-      }
-  }
+$('#number-search').on('submit', function(e) {
+  e.preventDefault()
+  const num = $('.menu-search').val()
+  const svgElement = document.getElementById('container')
+  const node = pm.getNodeByNumber(num)
+  console.log(node)
+
+  $('.content-container').animate({
+    scrollLeft: node.x - $(window).width() / 2
+  }, 100)
 })
+
 
 
 //miniMap(['24', '25'])
